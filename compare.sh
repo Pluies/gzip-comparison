64da35c041f43c844c9c41a63c6d245d4a565209
Florent Delannoy <florent.delannoy.external@ynap.com>
2019-10-02 17:59:00 +0100

    Add timings

--- compare.sh
+++ compare.sh
@@ -5,34 +5,39 @@ set -euo pipefail
 FILE=$1
 TMP=$(mktemp)
 
+function compare() {
+  PORT=$1
+  FILE=$2
+  curl -so /dev/null http://localhost:$PORT/$FILE --compressed -w '%{size_download}, Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total}\n'
+}
 echo "Comparing compression performance for file: $FILE (all sizes in bytes)"
 
 echo -n "origin, uncompressed      "
-curl -so /dev/null http://localhost:8000/$FILE --compressed -w '%{size_download}\n'
+compare 8000 $FILE
 
 echo -n "nginx, base               "
-curl -so /dev/null http://localhost:8001/$FILE --compressed -w '%{size_download}\n'
+compare 8001 $FILE
 
 echo -n "nginx, optimised          "
-curl -so /dev/null http://localhost:8002/$FILE --compressed -w '%{size_download}\n'
+compare 8002 $FILE
 
 echo -n "ambassador, base          "
-curl -so /dev/null http://localhost:8003/$FILE --compressed -w '%{size_download}\n'
+compare 8003 $FILE
 
 echo -n "ambassador, optimised     "
-curl -so /dev/null http://localhost:8004/$FILE --compressed -w '%{size_download}\n'
+compare 8004 $FILE
 
 echo -n "envoy, base               "
-curl -so /dev/null http://localhost:8005/$FILE --compressed -w '%{size_download}\n'
+compare 8005 $FILE
 
 echo -n "envoy, optimised          "
-curl -so /dev/null http://localhost:8006/$FILE --compressed -w '%{size_download}\n'
+compare 8006 $FILE
 
 echo -n "apache, base              "
-curl -so /dev/null http://localhost:8007/$FILE --compressed -w '%{size_download}\n'
+compare 8007 $FILE
 
 echo -n "apache, optimised         "
-curl -so /dev/null http://localhost:8008/$FILE --compressed -w '%{size_download}\n'
+compare 8008 $FILE
 
 echo -n "local gzip, base          "
 gzip --keep --stdout $FILE > $TMP && stat -f"%z" $TMP
