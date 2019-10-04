de04a4e40c35a6f75388475cf1c4201641d5ac27
Florent Delannoy <florent.delannoy.external@ynap.com>
2019-10-04 12:44:00 +0100

    Add more configs

--- compare.sh
+++ compare.sh
@@ -6,41 +6,42 @@ FILE=$1
 TMP=$(mktemp)
 
 function compare() {
-  PORT=$1
-  FILE=$2
+  SERVER="$1"
+  PORT="$2"
+  FILE="$3"
+  printf "%-40s" "$SERVER"
   curl -so /dev/null http://localhost:$PORT/$FILE --compressed -w '%{size_download}, Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total}\n'
 }
 echo "Comparing compression performance for file: $FILE (all sizes in bytes)"
 
-echo -n "origin, uncompressed      "
-compare 8000 $FILE
+compare "origin, uncompressed" 8000 $FILE
 
-echo -n "nginx, base               "
-compare 8001 $FILE
+compare "nginx, base" 8001 $FILE
 
-echo -n "nginx, optimised          "
-compare 8002 $FILE
+compare "nginx, optimised" 8002 $FILE
 
-echo -n "ambassador, base          "
-compare 8003 $FILE
+compare "ambassador, base" 8003 $FILE
 
-echo -n "ambassador, optimised     "
-compare 8004 $FILE
+compare "ambassador, optimised" 8004 $FILE
 
-echo -n "envoy, base               "
-compare 8005 $FILE
+compare "ambassador, optimised & window_bits" 8012 $FILE
 
-echo -n "envoy, optimised          "
-compare 8006 $FILE
+compare "envoy, base" 8005 $FILE
 
-echo -n "apache, base              "
-compare 8007 $FILE
+compare "envoy, optimised" 8006 $FILE
 
-echo -n "apache, optimised         "
-compare 8008 $FILE
+compare "envoy, optimised windonly" 8009 $FILE
 
-echo -n "local gzip, base          "
+compare "envoy, windowonly" 8010 $FILE
+
+compare "envoy, memory level only" 8011 $FILE
+
+compare "apache, base" 8007 $FILE
+
+compare "apache, optimised" 8008 $FILE
+
+echo -n "local gzip, base                        "
 gzip --keep --stdout $FILE > $TMP && stat -f"%z" $TMP
 
-echo -n "local gzip, optimised     "
+echo -n "local gzip, optimised                   "
 gzip --keep --stdout --best $FILE > $TMP && stat -f"%z" $TMP
